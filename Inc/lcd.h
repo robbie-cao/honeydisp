4f464ac2e06950333ed44e30d509b78567767b6a
Robbie Cao <robbie.cao@gmail.com>
2017-07-10 09:26:06 +0800

    Format code
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- Inc/lcd.h
+++ Inc/lcd.h
@@ -1,32 +1,32 @@
 #ifndef __LCD_H
-#define __LCD_H		
-#include "stdlib.h" 
+#define __LCD_H
+#include "stdlib.h"
 #include "stdint.h"
 
 typedef uint32_t  u32;
-typedef uint16_t u16;
-typedef uint8_t  u8;
+typedef uint16_t  u16;
+typedef uint8_t   u8;
 
 
-typedef volatile uint32_t  vu32;
+typedef volatile uint32_t vu32;
 typedef volatile uint16_t vu16;
 typedef volatile uint8_t  vu8;
 
-typedef struct  
-{		 	 
-	u16 width;			//LCD 宽度
-	u16 height;			//LCD 高度
-	u16 id;				//LCD ID
-	u8  dir;			//横屏还是竖屏控制：0，竖屏；1，横屏。	
-	u16 wramcmd;		//开始写gram指令
-	u16 setxcmd;		//设置x坐标指令
-	u16 setycmd;		//设置y坐标指令 
-}_lcd_dev; 	  
+typedef struct
+{
+  u16 width;      // LCD 宽度
+  u16 height;     // LCD 高度
+  u16 id;         // LCD ID
+  u8  dir;        // 横屏还是竖屏控制：0，竖屏；1，横屏。
+  u16 wramcmd;    // 开始写gram指令
+  u16 setxcmd;    // 设置x坐标指令
+  u16 setycmd;    // 设置y坐标指令
+}_lcd_dev;
 
 
-extern _lcd_dev lcddev;	//管理LCD重要参数
-extern u32  POINT_COLOR;       //默认红色    
-extern u32  BACK_COLOR;        //背景颜色.默认为白色
+extern _lcd_dev   lcddev;      // 管理LCD重要参数
+extern u32        POINT_COLOR; // 默认红色
+extern u32        BACK_COLOR;  // 背景颜色.默认为白色
 
 
 
@@ -38,92 +38,91 @@ extern u32  BACK_COLOR;        //背景颜色.默认为白色
 
 typedef struct
 {
-	vu16 LCD_REG;
-	vu16 LCD_RAM;
+  vu16 LCD_REG;
+  vu16 LCD_RAM;
 } LCD_TypeDef;
 
 
 #define LCD_BASE        ((u32)(0x60000000 | 0x0007E))
 #define LCD             ((LCD_TypeDef *) LCD_BASE)
 //////////////////////////////////////////////////////////////////////////////////
-	 
-//扫描方向定义
-#define L2R_U2D  0 		//从左到右,从上到下
-#define L2R_D2U  1 		//从左到右,从下到上
-#define R2L_U2D  2 		//从右到左,从上到下
-#define R2L_D2U  3 		//从右到左,从下到上
 
-#define U2D_L2R  4 		//从上到下,从左到右
-#define U2D_R2L  5 		//从上到下,从右到左
-#define D2U_L2R  6 		//从下到上,从左到右
-#define D2U_R2L  7		//从下到上,从右到左	 
+// 扫描方向定义
+#define L2R_U2D  0 		// 从左到右,从上到下
+#define L2R_D2U  1 		// 从左到右,从下到上
+#define R2L_U2D  2 		// 从右到左,从上到下
+#define R2L_D2U  3 		// 从右到左,从下到上
 
-#define DFT_SCAN_DIR  5  //默认的扫描方向
+#define U2D_L2R  4 		// 从上到下,从左到右
+#define U2D_R2L  5 		// 从上到下,从右到左
+#define D2U_L2R  6 		// 从下到上,从左到右
+#define D2U_R2L  7		// 从下到上,从右到左
 
-//画笔颜色
-#define WHITE         	 0xFFFF
-#define BLACK         	 0x0000	  
-#define BLUE         	 0x001F  
-#define BRED             0XF81F
-#define GRED 			 0XFFE0
-#define GBLUE			 0X07FF
-#define RED           	 0xF800
-#define MAGENTA       	 0xF81F
-#define GREEN         	 0x07E0
-#define CYAN          	 0x7FFF
-#define YELLOW        	 0xFFE0
-#define BROWN 			 0XBC40 //棕色
-#define BRRED 			 0XFC07 //棕红色
-#define GRAY  			 0X8430 //灰色
-//GUI颜色
+#define DFT_SCAN_DIR  5  // 默认的扫描方向
 
-#define DARKBLUE      	 0X01CF	//深蓝色
-#define LIGHTBLUE      	 0X7D7C	//浅蓝色  
-#define GRAYBLUE       	 0X5458 //灰蓝色
+// 画笔颜色
+#define WHITE         0xFFFF
+#define BLACK         0x0000
+#define BLUE          0x001F
+#define BRED          0xF81F
+#define GRED          0xFFE0
+#define GBLUE         0x07FF
+#define RED           0xF800
+#define MAGENTA       0xF81F
+#define GREEN         0x07E0
+#define CYAN          0x7FFF
+#define YELLOW        0xFFE0
+#define BROWN         0xBC40  // 棕色
+#define BRRED         0xFC07  // 棕红色
+#define GRAY          0x8430  // 灰色
 
- 
-#define LIGHTGREEN     	 0X841F //浅绿色
-#define LGRAY 		 0XC618 //浅灰色(PANNEL),窗体背景色
+// GUI颜色
+#define DARKBLUE      0x01CF  // 深蓝色
+#define LIGHTBLUE     0x7D7C	// 浅蓝色
+#define GRAYBLUE      0x5458  // 灰蓝色
 
-#define LGRAYBLUE        0XA651 //浅灰蓝色(中间层颜色)
-#define LBBLUE           0X2B12 //浅棕蓝色(选择条目的反色)
-	    															  
-void LCD_Init(void);													   	
-void LCD_DisplayOn(void);													
+
+#define LIGHTGREEN    0x841F  // 浅绿色
+#define LGRAY         0xC618  // 浅灰色(PANNEL),窗体背景色
+
+#define LGRAYBLUE     0xA651  // 浅灰蓝色(中间层颜色)
+#define LBBLUE        0x2B12  // 浅棕蓝色(选择条目的反色)
+
+void LCD_Init(void);
+void LCD_DisplayOn(void);
 void LCD_DisplayOff(void);
 void LCD_Switch_Off(void);
 void LCD_Switch_On(void);
 void LCD_Scroll_On(void);
-void LCD_Clear(u32 Color);	 												
-void LCD_SetCursor(u16 x1, u16 y1, u16 x2, u16 y2);										
-void LCD_DrawPoint(u16 x,u16 y);											
-void LCD_Fast_DrawPoint(u16 x,u16 y,u32 color);								
-u32  LCD_ReadPoint(u16 x,u16 y); 											
-void LCD_Draw_Circle(u16 x0,u16 y0,u8 r);						 			
-void LCD_DrawLine(u16 x1, u16 y1, u16 x2, u16 y2);							
-void LCD_DrawRectangle(u16 x1, u16 y1, u16 x2, u16 y2);		   				
-void LCD_Fill(u16 sx,u16 sy,u16 ex,u16 ey,u32 color);		   				
-void LCD_Color_Fill(u16 sx,u16 sy,u16 ex,u16 ey,u16 *color);				
-void LCD_ShowChar(u16 x,u16 y,u8 num,u8 size,u8 mode);						
-void LCD_ShowNum(u16 x,u16 y,u32 num,u8 len,u8 size);  						
-void LCD_ShowxNum(u16 x,u16 y,u32 num,u8 len,u8 size,u8 mode);				
-void LCD_ShowString(u16 x,u16 y,u16 width,u16 height,u8 size,u8 *p);		
+void LCD_Clear(u32 Color);
+void LCD_SetCursor(u16 x1, u16 y1, u16 x2, u16 y2);
+void LCD_DrawPoint(u16 x, u16 y);
+void LCD_Fast_DrawPoint(u16 x, u16 y, u32 color);
+u32  LCD_ReadPoint(u16 x, u16 y);
+void LCD_Draw_Circle(u16 x0, u16 y0, u8 r);
+void LCD_DrawLine(u16 x1, u16 y1, u16 x2, u16 y2);
+void LCD_DrawRectangle(u16 x1, u16 y1, u16 x2, u16 y2);
+void LCD_Fill(u16 sx, u16 sy, u16 ex, u16 ey, u32 color);
+void LCD_Color_Fill(u16 sx, u16 sy, u16 ex, u16 ey, u16 *color);
+void LCD_ShowChar(u16 x, u16 y, u8 num, u8 size, u8 mode);
+void LCD_ShowNum(u16 x, u16 y, u32 num, u8 len, u8 size);
+void LCD_ShowxNum(u16 x, u16 y, u32 num, u8 len, u8 size, u8 mode);
+void LCD_ShowString(u16 x, u16 y, u16 width, u16 height, u8 size, u8 *p);
 
 void LCD_WriteReg(u16 LCD_Reg, u16 LCD_RegValue);
 u16 LCD_ReadReg(u16 LCD_Reg);
 void LCD_WriteRAM_Prepare(void);
 void LCD_WriteRAM(u16 RGB_Code);
-void LCD_SSD_BackLightSet(u8 pwm);							
-void LCD_Scan_Dir(u8 dir);									
-void LCD_Display_Dir(u8 dir);								
-void LCD_Set_Window(u16 sx,u16 sy,u16 width,u16 height);	//设置窗口	
+void LCD_SSD_BackLightSet(u8 pwm);
+void LCD_Scan_Dir(u8 dir);
+void LCD_Display_Dir(u8 dir);
+void LCD_Set_Window(u16 sx, u16 sy, u16 width, u16 height);	//设置窗口
 
 void LCD_Delay(volatile unsigned int delay);
 
 
-#endif  
-	 
-	 
+#endif
+
 
 
 
